fbe1177c9f901a6226ddc2d812593c680a4103bc
hwangVLSI <136980856+hwangVLSI@users.noreply.github.com>
2025-11-10 23:09:53 +0000

    Deploying to gh-pages from @ hwangVLSI/hwangVLSI.github.io@f473ffb47e96767d2fad74a39d47f67766f803a2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-chip-gallery",
           title: "Chip Gallery",
-          description: "A collection of custom chips and FPGA prototypes.",
+          description: "A collection of custom chips prototypes.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/chip-gallery/";
