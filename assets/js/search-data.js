1c65a3a3e1353bb4bafb7336b21a6fce1d94d9e1
hwangVLSI <136980856+hwangVLSI@users.noreply.github.com>
2025-11-10 23:55:06 +0000

    Deploying to gh-pages from @ hwangVLSI/hwangVLSI.github.io@9c648ac4aac567f582beabc1515e61a3260bf4c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,16 +64,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-graduated-from-hit-gpa-86-06-100-rank-13-79",
+          title: 'I graduated from HIT!(GPA: 86.06/100, Rank 13/79)',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
